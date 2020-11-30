2b956f909644e5c07a1b2a64310aaffd2db17044
swebb461 <74932066+swebb461@users.noreply.github.com>
2020-11-30 15:16:24 -0500

    Update multiply-integer.py

--- multiply-integer.py
+++ multiply-integer.py
@@ -1,1 +1,10 @@
-
+def multiply_number():
+    """
+    :Takes two numbers, multiplies them, and prints the result as a string
+    """
+    a = input("type a number:")
+    b = input("type another:")
+    a = int(a)
+    b = int(b)
+    print(a * b)
+multiply_number()
