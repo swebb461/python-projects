488760f50f374fce3ed986d255655654496cd551
swebb461 <74932066+swebb461@users.noreply.github.com>
2020-12-01 14:28:30 -0500

    Update me.py

--- me.py
+++ me.py
@@ -1,1 +1,11 @@
 
+me = {"height": "6ft 2in",
+      "fav_color": "black",
+      "fav_author": "Hunter"}
+
+answer = input("Enter height, fav_color, or fav_author:")
+if answer in me:
+    result = me[answer]
+    print(result)
+
+
