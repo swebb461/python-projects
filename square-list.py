0832e3036d7c889a654ef30ffc1f5a645579fe09
swebb461 <74932066+swebb461@users.noreply.github.com>
2020-12-01 14:35:53 -0500

    Update square-list.py

--- square-list.py
+++ square-list.py
@@ -1,1 +1,15 @@
 
+class Square:
+    square_list = []
+    def __init__(self, s1):
+        self.s1 = s1
+
+    def calculate_perimeter(self):
+        return s1 * 4
+    
+    def print_size(self):
+        print("""{} by {} by {} by {}""".format(self.s1, self.s1, self.s1, self.s1))
+
+s1 = Square(20)
+s1.print_size()
+
