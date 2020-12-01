3471245c20d62fedfacca6c1aaf7eef42f6749d2
swebb461 <74932066+swebb461@users.noreply.github.com>
2020-12-01 14:15:13 -0500

    Update guess.py

--- guess.py
+++ guess.py
@@ -1,1 +1,14 @@
 
+qs = ["What number am i thinking of?",]
+
+num = ["28", "12", "14", "9"]
+
+while True:
+    answer = input("Guess a number or type v to quit")
+    if answer == "v":
+	    break
+    if answer in num:
+        print("You guessed correctly.")
+    else:
+        print("Wrong")
+
