bba99cf4eff42bdabf0d2bc9dd2d5338e84d1f0e
swebb461 <74932066+swebb461@users.noreply.github.com>
2020-12-01 14:09:43 -0500

    Update circle-area.py

--- circle-area.py
+++ circle-area.py
@@ -1,1 +1,9 @@
+import math
+class Circle:
+    def __init__(self, radius):
+	    self.radius = radius
+    def area(self):
+	    return self.radius**2 * math.pi
 
+a_circle = Circle(2)
+print(a_circle.area())
