5988f7e503eab3c329d8830440f3f08daf83462f
swebb461 <74932066+swebb461@users.noreply.github.com>
2020-12-01 14:35:38 -0500

    Update square-and-rectangle.py

--- square-and-rectangle.py
+++ square-and-rectangle.py
@@ -1,1 +1,24 @@
 
+class Shape:
+    def what_am_i(self):
+            print("I am a shape.")
+
+class Rectangle(Shape):
+    def __init__(self, w, l):
+	    self.width = w
+	    self.length = l
+    def calculate_perimeter(self):
+	    return self.width * 2 + self.length * 2
+    pass
+
+class Square(Shape):
+    def __init__(self, side):
+            self.side = side
+    def calculate_length(self):
+            return self.side * 4
+    def change_size(self, new_size):
+            self.side += new_size
+    pass
+a_square = Square(100)
+a_square.what_am_i()
+print(a_square.calculate_length())
