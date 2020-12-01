655d2a4efd71976a1e6f98924a6d97de7eccab8e
swebb461 <74932066+swebb461@users.noreply.github.com>
2020-12-01 14:26:04 -0500

    Update float.py

--- float.py
+++ float.py
@@ -1,7 +1,13 @@
 
-def f():
+def f(string):
     """
     :converts strings to floating-point numbers.
     """
-    return float()
+    try:
+        return float(string)
+    except ValueError:
+        print("could not convert string to a float")
 
+#to use, put any number inside the brackets remember to put quotes around the numbers!
+c = f("")
+print(c)
