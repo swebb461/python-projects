ed8ad337629bc97f3f720bb6133ac30d6710d9ca
swebb461 <74932066+swebb461@users.noreply.github.com>
2020-12-01 14:31:28 -0500

    Update response.py

--- response.py
+++ response.py
@@ -1,1 +1,7 @@
 
+input1 = input("what did you write?")
+input2 = input("where did you send it?")
+
+new_string = "Yesterday I wrote a {} and sent it to {}!".format(input1, input2)
+print(new_string)
+
