8a5f7421fab9ed1bf34c72cf06c92220f41f78f2
swebb461 <74932066+swebb461@users.noreply.github.com>
2020-12-01 14:15:46 -0500

    Update horse-and-rider.py

--- horse-and-rider.py
+++ horse-and-rider.py
@@ -1,1 +1,13 @@
 
+class Horse:
+    def __init__(self, name, breed, rider):
+        self.name = name
+        self.breed = breed
+        self.rider = rider
+class Rider:
+    def __init__(self, name):
+        self.name = name
+
+joe = Rider("Joe Mandela")
+rick = Horse("Ricky", "Bronco", joe)
+print(rick.rider.name)
